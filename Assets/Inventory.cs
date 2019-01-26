90c6d1c62abe487654655052a6cd4a166b8ca159
Luis E. Araujo B <luiseduardoab97@users.noreply.github.com>
2019-01-26 04:29:22 -0400

    Inventory logic

---
+++ Assets/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+    public Panel[] listPanel;
+
+    private void Start()
+    {
+        listPanel[0].activated();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            listPanel[0].activated();
+            desactivedAll(1);
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            listPanel[1].activated();
+            desactivedAll(2);
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            listPanel[2].activated();
+            desactivedAll(3);
+        } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            listPanel[3].activated();
+            desactivedAll(4);
+        } else if (Input.GetKeyDown(KeyCode.Alpha5)) {
+            listPanel[4].activated();
+            desactivedAll(5);
+        } else if (Input.GetKeyDown(KeyCode.Alpha6)) {
+            listPanel[5].activated();
+            desactivedAll(6);
+        } else if (Input.GetKeyDown(KeyCode.Alpha7)) {
+            listPanel[6].activated();
+            desactivedAll(7);
+        } else if (Input.GetKeyDown(KeyCode.Alpha8)) {
+            listPanel[7].activated();
+            desactivedAll(8);
+        } else if (Input.GetKeyDown(KeyCode.Alpha9)) {
+            listPanel[8].activated();
+            desactivedAll(9);
+        }
+    }
+
+    private void desactivedAll(int key)
+    {
+        
+        foreach (Panel panel in listPanel) {
+            Debug.Log("panel Id:"+panel.id);
+            Debug.Log("key Id:" + key);
+            if (panel.id != key)
+            {
+                panel.desactivated(); 
+            }
+        }
+    }
+}
