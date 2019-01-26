ea75128c1b7706a5450711634f7a7f884b70d154
gssantost <31294447+gssantost@users.noreply.github.com>
2019-01-26 02:01:11 -0400

    visual novel base

---
+++ Assets/Scripts/AutoType.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoType : MonoBehaviour {
+
+    public Text displayText;
+    public float letterPause = 0.2f;
+
+    private string message;
+
+    private enum States {
+        beginning, parents
+    }
+
+    private States currentState;
+
+    void Start() {
+        currentState = States.beginning;
+        //type(displayText.text);
+    }
+
+    void Update() {
+        switch (currentState) {
+            case States.beginning:
+                message = "En el medio de la tormenta y la bruma del desastre, ves todo lo preciado desvanecerse. " +
+                    "Lluvia y relámpagos a lo lejos, hacen crecer la caótica sinfonía. Estás sólo, con la vista hacía el cielo " +
+                    "impenetrable. Qué piensas?";
+                type(message);
+            break;
+            case States.parents:
+                message = "DBUGdudbsdcvdIOYSVDS";
+                type(message);
+            break;
+        }
+    }
+
+    public void setState(string state) {
+        
+    }
+
+    public void type(string m) {
+        message = m;
+        displayText.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText() {
+        foreach (char letter in message.ToCharArray()) {
+            displayText.text += letter;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+}
